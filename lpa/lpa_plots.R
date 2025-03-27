e41c3320d5bcdfde82315d2665060d124e311c72
krisjuune <kristiina.joon@gmail.com>
2025-03-27 14:50:43 +0100

    create plot for results of all three waves together

--- lpa/lpa_plots.R
+++ lpa/lpa_plots.R
@@ -28,30 +28,6 @@ lpa_data <- read_csv(
     )
   )
 
-process_lpa_data <- function(file_path) {
-  read_csv(file_path, show_col_types = FALSE)[, -1] |>
-    filter(!is.na(justice_class))
-}
-
-# read datasets
-wave1_data <- process_lpa_data(here("data", "lpa_output_w1.csv"))
-wave2_data <- process_lpa_data(here("data", "lpa_output_w2.csv"))
-wave3_data <- process_lpa_data(here("data", "lpa_output_w3.csv"))
-
-# set factor levels for justice_class based on results
-wave1_data <- wave1_data |>
-  mutate(justice_class = factor(justice_class, levels = c("1", "2", "3"), labels = c("Universalists", "Egalitarianists", "Utilitarianists")))
-
-wave2_data <- wave2_data |>
-  mutate(justice_class = factor(justice_class, levels = c("1", "2", "3"), labels = c("Universalists", "Egalitarianists", "Utilitarianists")))
-
-wave3_data <- wave3_data |>
-  mutate(justice_class = factor(justice_class, levels = c("1", "2", "3"), labels = c("Universalists", "Egalitarianists", "Utilitarianists")))
-
-# list datasets
-datasets <- list(wave1 = wave1_data, wave2 = wave2_data, wave3 = wave3_data)
-
-
 # get proportion table and median scores
 justice_class_proportions <- as.list(prop.table(table(lpa_data$justice_class)))
 
@@ -220,62 +196,64 @@ plot_participant_profiles <- function(data) {
       strip.text.x = element_text(size = main_text_size, face = "bold")
     )
 
-  plot
+  return(plot)
 }
 
-# create raincloud plot
-plot_raincloud <- lpa_data |>
-  pivot_participant_profiles_long() |>
-  mutate(
-    justice_class_prop = case_when(
-      justice_class == "Egalitarianists" ~ paste0("Egalitarianists (", percent(justice_class_proportions$Egalitarian, accuracy = 0.1), ")"),
-      justice_class == "Universalists" ~ paste0("Universalists (", percent(justice_class_proportions$Universal, accuracy = 0.1), ")"),
-      justice_class == "Utilitarianists" ~ paste0("Utilitarianists (", percent(justice_class_proportions$Utilitarian, accuracy = 0.1), ")"),
+## get raincloud plots
+plot_raincloud <- function(data, main_text_size = 12) {
+  justice_class_proportions <- as.list(prop.table(table(data$justice_class)))
+  
+  data |>
+    pivot_participant_profiles_long() |>
+    mutate(
+      justice_class_prop = case_when(
+        justice_class == "Egalitarianists" ~ paste0("Egalitarianists (", percent(justice_class_proportions$Egalitarian, accuracy = 0.1), ")"),
+        justice_class == "Universalists" ~ paste0("Universalists (", percent(justice_class_proportions$Universal, accuracy = 0.1), ")"),
+        justice_class == "Utilitarianists" ~ paste0("Utilitarianists (", percent(justice_class_proportions$Utilitarian, accuracy = 0.1), ")")
+      )
+    ) |>
+    ggplot(aes(
+      x = principle,
+      y = value,
+      fill = justice_class_prop,
+      colour = justice_class_prop
+    )) +
+    stat_slab(
+      alpha = .6,
+      adjust = 5,
+      width = .6,
+      .width = c(.5, .95),
+      density = "bounded"
+    ) +
+    geom_boxplot(
+      width = .15,
+      fill = "white",
+      outlier.shape = NA
+    ) +
+    # geom_half_point(
+    #   side = "l",
+    #   range_scale = .4,
+    #   alpha = .2
+    # ) +
+    coord_flip() +
+    scale_y_continuous(breaks = seq(0, 15, 1)) +
+    scale_fill_viridis_d(end = .8) +
+    scale_colour_viridis_d(end = .8) +
+    facet_wrap(~justice_class_prop, ncol = 1) +
+    labs(
+      y = "Sum score",
+      x = "Justice principle",
+      colour = NULL,
+      fill = NULL
+    ) +
+    theme_classic() +
+    theme(
+      legend.position = "bottom",
+      text = element_text(size = main_text_size),
+      strip.background = element_rect(size = 0),
+      strip.text.x = element_text(size = main_text_size, face = "bold")
     )
-  ) |>
-  ggplot(aes(
-    x = principle,
-    y = value,
-    fill = justice_class_prop,
-    colour = justice_class_prop
-  )) +
-  stat_halfeye(
-    alpha = .6,
-    adjust = 2.2,
-    width = .6,
-    .width = c(.5, .95),
-    density = "bounded"
-  ) +
-  geom_boxplot(
-    width = .15,
-    fill = "white",
-    outlier.shape = NA
-  ) +
-  geom_half_point(
-    side = "l",
-    range_scale = .4,
-    alpha = .2
-  ) +
-  coord_flip() +
-  scale_y_continuous(breaks = seq(0, 15, 1)) +
-  scale_fill_viridis_d(end = .8) +
-  scale_colour_viridis_d(end = .8) +
-  facet_wrap(~justice_class_prop, ncol = 1) +
-  labs(
-    y = "Sum score",
-    x = "Justice principle",
-    colour = NULL,
-    fill = NULL
-  ) +
-  theme_classic() +
-  theme(
-    legend.position = "none",
-    text = element_text(size = main_text_size),
-    strip.background = element_rect(size = 0),
-    strip.text.x = element_text(size = main_text_size, face = "bold")
-  )
-
-
+}
 
 # get plots
 plot_lpa_results <- plot_lpa_results(lpa_data)
@@ -286,6 +264,8 @@ plot_participants <- lpa_data |>
   pivot_participant_profiles_long(shorten_labels = TRUE) |>
   plot_participant_profiles() +
   facet_wrap(~justice_class, ncol = 3)
+
+plot_raincloud <- plot_raincloud(lpa_data)
 
 # check plots
 plot_raincloud
@@ -318,4 +298,122 @@ write_csv(
 write_csv(
   principles_summary,
   here("data", "lpa_principle_support.csv")
+)
+
+
+
+################ test wave by wave #######################
+
+process_lpa_data <- function(file_path) {
+  read_csv(file_path, show_col_types = FALSE)[, -1] |>
+    filter(!is.na(justice_class))
+}
+
+# Process datasets
+wave1_data <- process_lpa_data(here("data", "lpa_output_w1.csv"))
+wave2_data <- process_lpa_data(here("data", "lpa_output_w2.csv"))
+wave3_data <- process_lpa_data(here("data", "lpa_output_w3.csv"))
+
+# MANUALLY set factor levels for justice_class based on results
+wave1_data <- wave1_data |>
+  mutate(justice_class = factor(justice_class, levels = c("1", "3", "2"), labels = c("Egalitarianists", "Universalists", "Utilitarianists")))
+
+wave2_data <- wave2_data |>
+  mutate(justice_class = factor(justice_class, levels = c("2", "3", "1"), labels = c("Egalitarianists", "Universalists", "Utilitarianists")))
+
+wave3_data <- wave3_data |>
+  mutate(justice_class = factor(justice_class, levels = c("1", "3", "2"), labels = c("Egalitarianists", "Universalists", "Utilitarianists")))
+
+# List of datasets for iteration
+datasets <- list(wave1 = wave1_data, wave2 = wave2_data, wave3 = wave3_data)
+
+# Analyze and plot results for each dataset
+results <- lapply(datasets, function(data) {
+  justice_class_proportions <- as.list(prop.table(table(data$justice_class)))
+
+  list(
+    proportions = justice_class_proportions,
+    summary = data |>
+      summarise(across(c(utilitarian, egalitarian, sufficientarian, limitarian), 
+                       list(Median = median, Mean = mean, SD = sd, Support = ~ mean(. > 7) * 100), 
+                       .names = "{.col}_{.fn}")) |>
+      pivot_longer(cols = everything(), names_to = c("Column", "Statistic"), names_sep = "_") |>
+      pivot_wider(names_from = "Statistic", values_from = "value"),
+    profile_plots = data |>
+      group_by(justice_class) |>
+      slice_sample(n = 50) |>
+      pivot_participant_profiles_long(shorten_labels = TRUE) |>
+      plot_participant_profiles() +
+      facet_wrap(~justice_class, ncol = 3),
+    raincloud_plots = plot_raincloud(data, justice_class_proportions)
+  )
+})
+
+# Access results per wave
+wave1_results <- results$wave1
+wave2_results <- results$wave2
+wave3_results <- results$wave3
+
+ggsave(
+  here("output", "wave1_lpa_profiles.png"),
+  plot = wave1_results$profile_plots,
+  height = 6, width = 11
+)
+
+ggsave(
+  here("output", "wave1_lpa_raincloud.png"),
+  plot = wave1_results$raincloud_plots,
+  height = 6, width = 11
+)
+
+ggsave(
+  here("output", "wave2_lpa_profiles.png"),
+  plot = wave2_results$profile_plots,
+  height = 6, width = 11
+)
+
+ggsave(
+  here("output", "wave2_lpa_raincloud.png"),
+  plot = wave2_results$raincloud_plots,
+  height = 6, width = 11
+)
+ggsave(
+  here("output", "wave3_lpa_profiles.png"),
+  plot = wave3_results$profile_plots,
+  height = 6, width = 11
+)
+
+ggsave(
+  here("output", "wave3_lpa_raincloud.png"),
+  plot = wave3_results$raincloud_plots,
+  height = 6, width = 11
+)
+
+datasets_long <- bind_rows(
+  lapply(seq_along(datasets), function(i) {
+    datasets[[i]] %>%
+      mutate(dataset = names(datasets)[i])
+  })
+)
+
+raincloud_plot_waves <- plot_raincloud(datasets_long) +
+  facet_wrap(~dataset, ncol = 3) +
+  labs(
+    colour = NULL,
+    shape = NULL,
+    y = NULL,
+    x = "Marginal means"
+  ) +
+  theme_classic() +
+  theme(
+    legend.position = "right",
+    text = element_text(size = 11),
+    strip.background = element_rect(size = 0),
+    strip.text.x = element_text(size = 11, face = "bold")
+  )
+
+ggsave(
+  here("output", "waves_lpa_raincloud.png"),
+  plot = raincloud_plot_waves,
+  height = 6, width = 11
 )
